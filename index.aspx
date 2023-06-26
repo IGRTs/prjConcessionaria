30d7c1e104939b46ce8eec3fb77c5a9d79422dc2
IGRT <71153494+IgorTeodoro@users.noreply.github.com>
2023-06-25 21:49:26 -0300

    DataList Carros

--- index.aspx
+++ index.aspx
@@ -4,7 +4,29 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ConteudoWebsite" Runat="Server">
 
+    <div class="jumbotron">
+    </div>
 
+    <div class="row">
+        <asp:DataList ID="DataList1" runat="server" DataKeyField="cod_carro" DataSourceID="SqlDataSource1" OnItemDataBound="DataList1_ItemDataBound">
+            <ItemTemplate>
+                <div class="col-md-4">
+                    <div class="card" style="width: 18rem;">
+                        <asp:Image  ID="imgProduto" class="card-img-top" ImageUrl='<%# Eval("imagem")%>' runat="server" Height="250px" Width="300px" />
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <asp:Label ID="nomeLabel" runat="server" Text='<%# Eval("nome") %>' /></h5>
+                            <p class="card-text">
+                                <asp:Label ID="descricaoLabel" runat="server" Text='<%# Eval("descricao") %>' />
+                            </p>
+                            <asp:Button class="btn btn-primary" OnClick="btnDetalhes_Click" ID="btnDetalhes" runat="server" Text="Ver Detalhes" />
+                        </div>
+                    </div>
+                </div>
+            </ItemTemplate>
+        </asp:DataList>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConcessionariaDBConnectionString %>" SelectCommand="SELECT * FROM [carrovrumvrum]"></asp:SqlDataSource>
+    </div>
 
 </asp:Content>
 
--- index.aspx.cs
+++ index.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -10,5 +11,48 @@ public partial class index : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void DataList1_ItemDataBound(object sender, DataListItemEventArgs e)
+    {
+
+    }
+
+    protected void btnDetalhes_Click(object sender, EventArgs e)
+    {
+        Button btnDetalhes = (Button)sender; // Obtém o botão que disparou o evento
+
+        // Obtém a referência para o item do DataList
+        DataListItem item = (DataListItem)btnDetalhes.NamingContainer;
+
+        // Obtém os dados associados ao item do DataList
+        DataRowView dataItem = (DataRowView)item.DataItem;
+
+        // Acessa os valores das colunas do DataRowView
+        string nome = dataItem["nome"].ToString();
+        string imagem = dataItem["imagem"].ToString();
+        string modelo = dataItem["modelo"].ToString();
+        string combustivel = dataItem["combustivel"].ToString();
+        string cor = dataItem["cor"].ToString();
+        string ano = dataItem["ano"].ToString();
+        string km = dataItem["km"].ToString();
+        string descricao = dataItem["descricao"].ToString();
+        string preco = dataItem["preco"].ToString();
+
+        Session["infoCompleta"] = new
+        {
+            nomeCarro = nome,
+            imagemCarro = imagem,
+            modeloCarro = modelo,
+            combustivelCarro = combustivel,
+            corCarro = cor,
+            anoCarro = ano,
+            kmCarro = km,
+            descricaoCarro = descricao,
+            precoCarro = preco
+        };
+
+
+        Response.Redirect("OutraPagina.aspx");
     }
 }
